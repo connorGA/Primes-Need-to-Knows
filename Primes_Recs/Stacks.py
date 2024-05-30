afd916b47976a4dd050faa54744cc3c25a1b8bd0
connorGA <ronnoccap@gmail.com>
2024-05-29 21:16:20 -0700

    Stacks: basic operations

---
+++ Primes_Recs/Stacks.py
@@ -0,0 +1,7 @@
+# A stack is a linear data structure that follows the "last in first out"(LIFO) principle. This means, the last element added to the stack is the first one to be removed. Stacks are used in various applications, like function call management, expression evalutaion, and back tracking algorthims.
+
+# Basic Operations
+    # Push: Add an element to the top of the stack
+    # Pop: remove the top element from the stack
+    # Peek/Top: retrieve the top element of the stack without removing it
+    # isEmpty: check if the stack is empty
