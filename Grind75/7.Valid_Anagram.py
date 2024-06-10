3523fb999a505a6c96255cf3887d1e5d4d5b8807
connorGA <ronnoccap@gmail.com>
2024-06-10 15:49:59 -0700

    Grind75: Problem #7 - Valid Anagram

---
+++ Grind75/7.Valid_Anagram.py
@@ -0,0 +1,31 @@
+# Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+# An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+
+
+# Example 1:
+
+# Input: s = "anagram", t = "nagaram"
+# Output: true
+# Example 2:
+
+# Input: s = "rat", t = "car"
+# Output: false
+
+class Solution(object):
+    def isAnagram(self, s, t):
+        if len(s) != len(t):
+            return False
+        
+        countS, countT = {}, {}
+
+        for i in range(len(s)):
+            countS[s[i]] = 1 + countS.get(s[i], 0)
+            countT[t[i]] = 1 + countT.get(t[i], 0)
+        for c in countS:
+            if countS[c] != countT.get(c, 0):
+                return False
+        return True
+    
+# Big O Notation:
+    # Time: O(n) - where n is the length of the longest string
+    # Space: O(n) - where n is the length of the longest string
