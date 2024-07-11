96f844532fbd9a93559d19b3ad188f0299e4977d
connorGA <ronnoccap@gmail.com>
2024-07-11 10:59:51 -0700

    Grind75: Problem #32 - Clone Graph

--- Grind75/31.Binary_Tree_Level_Order_Traversal.py
+++ Grind75/31.Binary_Tree_Level_Order_Traversal.py
@@ -18,3 +18,23 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+from collections import deque
+
+class Solution(object):
+    def levelOrder(self, root):
+        res = []
+        q = deque()
+        q.append(root)
+
+        while q:
+            qLen = len(q)
+            level = []
+            for i in range(qLen):
+                node = q.popleft()
+                if node:
+                    level.append(node.val)
+                    q.append(node.left)
+                    q.append(node.right)
+            if level:
+                res.append(level)
+        return res
