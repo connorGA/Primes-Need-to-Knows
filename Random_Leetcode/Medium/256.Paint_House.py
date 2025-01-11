6b08f41e65418aaa4341b94ad73e77b9cece9da0
Connor Cappello <ronnoccap@gmail.com>
2025-01-11 10:14:18 -0800

    Random Leetcode(Medium) - 256. Paint House FIX, previous solution didn't account for adjacent house constraint

--- Random_Leetcode/Medium/256.Paint_House.py
+++ Random_Leetcode/Medium/256.Paint_House.py
@@ -16,7 +16,7 @@
 # Input: costs = [[7,6,2]]
 # Output: 2
 
-class Solution(object):
+class Solution(object):                     ##### WRONG SOLUTION - DOESNT ACCOUNT FOR ADJACENT HOUSE CONSTRAINT
     def minCost(self, costs):
         minPrice = 0
         for row in costs:
@@ -32,3 +32,24 @@ class Solution(object):
     # Time: O(m x n) - the method needs run through every array in every row
     # Space: O(1) - minPrice and min are scalar variables, no additional space is required
                 
+
+
+# CORRECT SOLUTION - PASSES ALL TEST CASES
+class Solution(object):
+    def minCost(self, costs):
+        if not costs:
+            return 0
+        
+        # Iterate from the second-to-last house to the first house
+        for i in range(len(costs) - 2, -1, -1):
+            # Update the current house cost by adding the minimum cost of painting the next house
+            costs[i][0] += min(costs[i + 1][1], costs[i + 1][2])  # Red
+            costs[i][1] += min(costs[i + 1][0], costs[i + 1][2])  # Blue
+            costs[i][2] += min(costs[i + 1][0], costs[i + 1][1])  # Green
+        
+        # Return the minimum cost of painting the first house
+        return min(costs[0][0], costs[0][1], costs[0][2])
+
+# Big O Notation:
+    # Time: O(n) - where n is the number of houses
+    # Space: O(1) - modifes the costs matrix in place without using additional space
